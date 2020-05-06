29b270ae7b31a5eda26d6f39bd46b3565b3762d4
Jasper van Nieuwenhuizen <jaspervannieuwenhuizen@gmail.com>
2020-05-06 17:49:14 +0200

    Tweak colors and line height

--- config.h
+++ config.h
@@ -10,15 +10,15 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeNorm] = { "#8ca0aa", "#282c33" },
+	[SchemeSel] = { "#8ca0aa", "#2e343f" },
+	[SchemeSelHighlight] = { "#ffc978", "#2e343f" },
+	[SchemeNormHighlight] = { "#ffc978", "#282c33" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 30;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 40;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
